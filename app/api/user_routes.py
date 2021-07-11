6534debb131656ac244dae7dbfa3875a6ce826cb
sdkag <senyo.k.agawu@gmail.com>
2021-07-11 03:19:41 -0400

    remove choicestype column and enum all together. plan is to validate through the forms, or maybe custom validate the init method?

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -9,11 +9,29 @@ user_routes = Blueprint('users', __name__)
 @login_required
 def users():
     users = User.query.all()
-    return {"users": [user.to_dict() for user in users]}
+
+    [stoods, tas] = [[], []]
+    for u in users:
+        if u.discriminant == 'stood':
+            stoods.append(u)
+        else:
+            tas.append(u)
+
+    return {
+        "users": [user.to_dict() for user in users],
+        "stoods": [s.to_dict() for s in stoods],
+        "tas": [t.to_dict() for t in tas]
+        }
 
 
 @user_routes.route('/<int:id>')
 @login_required
 def user(id):
     user = User.query.get(id)
+    return user.to_dict()
+
+@user_routes.route('/me')
+@login_required
+def current_user():
+    current_user
     return user.to_dict()
--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,8 @@
+from sqlalchemy.sql.expression import desc
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
+from sqlalchemy.dialects.postgresql import ENUM
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -9,15 +10,43 @@ class User(db.Model, UserMixin):
         'polymorphic_identity': 'user',
         'polymorphic_on': 'discriminant'
     }
-    #TODO OAuth with github
-    id = db.Column(db.Integer, primary_key = True)
-    #github username
-    username = db.Column(db.String(40), nullable = False, unique = True)
-    #github email
-    email = db.Column(db.String(255), nullable = False, unique = True)
-    hashed_password = db.Column(db.String(255), nullable = False)
 
-    discriminant = db.Column(db.String(10), nullable)
+    DISCRIMINANT_TYPES = [
+        # key, value
+        ('stood', 'Student'),
+        ('ta', 'Teaching Assistant')
+    ]
+    # TODO OAuth with github
+    id = db.Column(db.Integer, primary_key=True)
+    # github username
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    # github email
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
+    discriminant = db.Column(db.String(55), nullable=False)
+
+    @property
+    def related_role(self):
+        # can be built out to be a switch case fi there are more types of user.
+        return self.stood if self.discriminant('stood') else self.ta
+    # ChoiceTypes, is essentially ENUM apparently there's some spiffy locale stuff you can do with this, but i don't really get locale
+    # ChoiceType defaults to be a Unicode(255) column which is why in the docs you see u'stood' instead of 'stood', default is just that, so we only have to specify for ta's.
+    # # the [0][0] nonsense is to keep it dynamic.
+
+    stood = db.relationship(
+        'Stood',
+        uselist=False,
+        backref=(db.backref('user', uselist=False)),
+        lazy='joined'
+        )
+
+    ta = db.relationship(
+        'TA',
+        uselist=False,
+        backref=(db.backref('user', uselist=False)),
+        lazy='joined'
+        )
+
     @property
     def password(self):
         return self.hashed_password
@@ -51,6 +80,14 @@ class Stood(User):
     heroku_url = db.Column(db.String)
     scorecard_url = db.Column(db.String)
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "heroku_url": self.heroku_url,
+            "scorecard_url": self.scorecard_url
+        }
 
 
 class TA(User):
@@ -63,3 +100,6 @@ class TA(User):
         db.ForeignKey('users.id'),
         primary_key=True
         )
+
+    def to_dict(self):
+        return super().to_dict()
